7728e71f207aa5a49724613277f8de5274fb3063
adithkrishnaor <adithkrishnaor@gmail.com>
2024-07-03 21:45:10 +0530

    login page

---
+++ lib/login_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Color.fromARGB(165, 16, 130, 172),
+          title: const Text('Login App')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Login Here',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 0, 0, 0)
+                  ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    hintText: 'Username',
+                    filled: true,
+                    fillColor: Color.fromARGB(43, 255, 255, 255)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    hintText: 'Password',
+                    filled: true,
+                    fillColor: Color.fromARGB(43, 255, 255, 255)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    checkLogin(context);
+                  },
+                  child: const Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                      backgroundColor: Color.fromARGB(255, 60, 171, 231),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void checkLogin(BuildContext ctx) {
+    final _username = _usernameController.text;
+    final _password = _passwordController.text;
+
+    if (_username == _password) {
+      // go to home
+    } else {
+      final _errormessage = "Username and Password doesn't match";
+
+      //can use 3 types of messages : SnackBar, Alert Dilaogue , Simple Text
+
+      ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+        behavior: SnackBarBehavior.floating,
+        margin: EdgeInsets.all(10),
+        content: Text(_errormessage),
+        duration: Duration(seconds: 3),
+      ));
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:first_flutter_app/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 173, 203, 158)
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
