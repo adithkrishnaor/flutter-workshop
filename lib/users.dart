00fd84baad4d0b096903d1b34c0a6b5efc19cd16
adithkrishnaor <adithkrishnaor@gmail.com>
2024-07-16 22:01:42 +0530

    refresh added , button edited

--- lib/users.dart
+++ lib/users.dart
@@ -107,9 +107,11 @@ class _UsersScreenState extends State<UsersScreen> {
               Row(children: [
                  const SizedBox(width: 50),
                 TextButton(
-                  style: const ButtonStyle(
+                  style: ButtonStyle(
                     shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(),
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
                     ),
                     backgroundColor: WidgetStatePropertyAll(
                       Color.fromARGB(255, 165, 27, 27),
@@ -129,15 +131,19 @@ class _UsersScreenState extends State<UsersScreen> {
                 ),
                 const SizedBox(width: 50),
                 TextButton(
-                  style: const ButtonStyle(
+                  style: ButtonStyle(
                     shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(),
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
                     ),
                     backgroundColor: WidgetStatePropertyAll(
                       Color.fromARGB(255, 78, 153, 27),
                     ),
                   ),
-                  onPressed: () {},
+                  onPressed: (){
+                    refreshUser();
+                  },
                   child: const Text(
                     'Refresh',
                     style: TextStyle(
@@ -166,4 +172,8 @@ class _UsersScreenState extends State<UsersScreen> {
       ),
     );
   }
+  void refreshUser() async {
+  userdata = await getAllUsers();
+  setState(() {});
+}
 }
